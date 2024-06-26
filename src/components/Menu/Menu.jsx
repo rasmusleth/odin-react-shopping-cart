f8dec428fe2aba1af75661a7379d07885110c059
Rasmus Leth <rasmus@rasmusleth.dk>
2024-06-26 11:03:36 +0200

    Install prop-types, setup/adjust rough version of menu page, including css

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,125 @@
-const Menu = () => {
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styles from "./menu.module.css";
+
+const MenuItem = ({ item }) => {
+  const itemsInCart = { 0: 5, 3: 2 };
+
+  return (
+    <>
+      {item.id.toString() in itemsInCart ? (
+        <a href="" className={(styles.menuItemWrapper, styles.added)}>
+          <div className={styles.menuItemContent}>
+            <h3>{item.name}</h3>
+            <p className={styles.textStyle2Lines}>{item.description}</p>
+            <p className={styles.textColorPrimary}>{item.priceFormatted}</p>
+          </div>
+          <img
+            className={styles.menuItemImage}
+            src="/public/images/menu-items/luksus-smoerrebroed-cover.jpeg"
+            alt={item.name}
+          />
+          <div className={styles.menuItemAddedQuantity}>
+            {itemsInCart[item.id]}
+          </div>
+        </a>
+      ) : (
+        <a href="" className={styles.menuItemWrapper}>
+          <div className={styles.menuItemContent}>
+            <h3>{item.name}</h3>
+            <p className={styles.textStyle2Lines}>{item.description}</p>
+            <p className={styles.textColorPrimary}>{item.priceFormatted}</p>
+          </div>
+          <img
+            className={styles.menuItemImage}
+            src="/public/images/menu-items/luksus-smoerrebroed-cover.jpeg"
+            alt={item.name}
+          />
+        </a>
+      )}
+      <hr />
+    </>
+  );
+};
+
+const CategorySection = ({ category }) => {
+  console.log(category);
+  return (
+    <>
+      <section
+        className={styles.menuCategorySection}
+        id={category.slug.slice(1)}
+      >
+        <div className={styles.menuCategoryTitleContainer}>
+          <a
+            href={`#${category.slug.slice(1)}`}
+            className={styles.menuCategoryTitleWrapper}
+          >
+            <img
+              className={styles.menuCategoryTitleImage}
+              src="/public/images/menu-items/smoerrebroed-aeg.jpeg"
+              alt=""
+            />
+            <h2 className={styles.menuCategoryTitle}>{category.title}</h2>
+          </a>
+        </div>
+        <div className={styles.menuItemList}>
+          {category.items.map((item) => (
+            <MenuItem key={`categoryItem-${item.id}`} item={item} />
+          ))}
+        </div>
+      </section>
+      <div className={styles.spacerMedium}></div>
+    </>
+  );
+};
+
+const MenuCategorySliderItem = ({ category }) => {
+  return (
+    <a
+      href={`#${category.slug.slice(1)}`}
+      className={styles.menuCategoriesSliderItem}
+    >
+      {category.title}
+    </a> // old class name: menu_categories-item
+  );
+};
+
+const Menu = ({ categories }) => {
   return (
     <>
       <h1>Menu</h1>
       <p>Browse our collection of delicious meals</p>
+      <br />
+      <br />
+      <div className={styles.menuPageWrapper}>
+        {/* <div class="spacer-small"></div> */}
+        <div className={styles.menuCategoriesSlider}>
+          {categories.map((category) => (
+            <MenuCategorySliderItem
+              key={`slider-${category.id}`}
+              category={category}
+            />
+          ))}
+        </div>
+        <div className={styles.spacerSmall}></div>
+
+        {categories.map((category) => (
+          <CategorySection key={`section-${category.id}`} category={category} />
+        ))}
+      </div>
+
+      <div className="dynamic-height-element"></div>
+
+      <dialog className={styles.itemDialog}>
+        {/* <%- include("menu_item", {menu_item: null}) %> */}
+      </dialog>
     </>
   );
+};
+
+Menu.propTypes = {
+  categories: PropTypes.array,
 };
 
 export default Menu;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,233 @@ import Menu from "./components/Menu/Menu.jsx";
 import Home from "./components/Home/Home.jsx";
 import App from "./App.jsx";
 
+const categoriesData = [
+  {
+    id: 0,
+    slug: "/category0",
+    title: "Category 0",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 1,
+    slug: "/category1",
+    title: "Category 1",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 2,
+    slug: "/category2",
+    title: "Category 2",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 3,
+    slug: "/category3",
+    title: "Category 3",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 4,
+    slug: "/category4",
+    title: "Category 4",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 5,
+    slug: "/category5",
+    title: "Category 5",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 6,
+    slug: "/category6",
+    title: "Category 6",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+  {
+    id: 7,
+    slug: "/category7",
+    title: "Category 7",
+    items: [
+      {
+        id: 1000,
+        name: "Food item 0",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1001,
+        name: "Food item 1",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+      {
+        id: 1002,
+        name: "Food item 2",
+        description:
+          "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
+        priceFormatted: "85,00 DKK",
+      },
+    ],
+  },
+];
+
 const routes = [
   {
     path: "/",
@@ -13,7 +240,7 @@ const routes = [
       },
       {
         path: "/menu",
-        element: <Menu />,
+        element: <Menu categories={categoriesData} />,
       },
     ],
   },
