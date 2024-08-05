08bbe55b7bf57322a28d15466ec5b32c9bfa9b30
Rasmus Leth <rasmus@rasmusleth.dk>
2024-08-05 17:29:03 +0200

    Remove nav and CartContext from ErrorPage

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,60 +1,28 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Navigation from "./components/Navigation/Navigation";
-import {
-  lightModeColors,
-  darkModeColors,
-} from "./assets/javascript/colorTheme";
-import { CartProvider } from "./components/Cart/CartContext";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // # Color Theme
-  const toggleColorTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  useEffect(() => {
-    if (isDarkMode) {
-      for (let colorKey in darkModeColors) {
-        document.documentElement.style.setProperty(
-          colorKey,
-          darkModeColors[colorKey]
-        );
-      }
-    } else {
-      for (let colorKey in lightModeColors) {
-        document.documentElement.style.setProperty(
-          colorKey,
-          lightModeColors[colorKey]
-        );
-      }
-    }
-  }, [isDarkMode]);
+  const error = useRouteError();
+  console.error(error);
 
   return (
-    <CartProvider>
-      <>
-        <Navigation
-          isDarkMode={isDarkMode}
-          onColorThemeClick={toggleColorTheme}
-        />
-        <div className="errorPageWrapper">
-          <div>
-            <h1 className="heading-style-h3">Sorry, page not found</h1>
-            <div className="spacerSmall"></div>
-            <p>We cannot find the page you are looking for..</p>
-            <div className="spacerXSmall"></div>
-            <p>Looking for the menu? Click the button below</p>
-          </div>
-          <div className="spacerSmall"></div>
-          <Link to="/" className="btn btnPrimary">
-            Go to menu
-          </Link>
-        </div>
-      </>
-    </CartProvider>
+    <div className="errorPageWrapper">
+      <div>
+        <h1 className="heading-style-h3">Sorry, page not found</h1>
+        <div className="spacerSmall"></div>
+        <p>We cannot find the page you are looking for..</p>
+        <div className="spacerXSmall"></div>
+        <p>Looking for the menu? Click the button below</p>
+      </div>
+      <div className="spacerSmall"></div>
+      <Link to="/" className="btn btnPrimary">
+        Go to menu
+      </Link>
+      <div className="spacerSmall"></div>
+      <p>
+        Error text: <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
   );
 };
 
